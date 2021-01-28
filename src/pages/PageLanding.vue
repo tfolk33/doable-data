5eeaee17aef025c95ccabb156f46e92dd14f540d
Tanner Folkman <tfolkman33@gmail.com>
2021-01-28 13:39:54 -0500

    some changes

--- src/pages/PageLanding.vue
+++ src/pages/PageLanding.vue
@@ -1,11 +1,5 @@
 <template>
-  <div  class="fullscreen bg-white text-white q-pa-md">
-    <q-img
-      :src="url"
-      spinner-color="white"
-      style="height: 140px; max-width: 150px"
-    />
-
+  <div  class="fullscreen bg-blue text-white q-pa-md">
     <div class="text-h2 q-ma-md" style="opacity:.6">
         We are committed to bring your business into the digital age by creating and managing your forms to fill out yourself or send off for completion.
     </div>
@@ -24,7 +18,7 @@ export default {
   name: 'Landing',
   data () {
     return {
-      url: 'C:\\Users\\TFolk\\repos\\Quasar\\doable-data\\src\\DoableData.png'
+      url: 'https://github.com/tfolk33/doable-data/blob/master/src/DoableData.png'
     }
   }
 }
